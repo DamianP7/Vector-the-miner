cd36367993074a4fa46ad21b30c714b51fc9d868
DamianP7 <damian7it@gmail.com>
2019-05-27 15:08:59 +0200

    Added custom inspectors in Prices assets.

---
+++ Assets/Editor/ItemPricesEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ItemsPrices))]
+public class ItemPricesEditor : Editor
+{
+	ItemsPrices myTarget;
+	protected List<bool> itemsFolded;
+
+	protected void OnEnable()
+	{
+		myTarget = (ItemsPrices)target;
+		itemsFolded = new List<bool>();
+		for (int i = 0; i < myTarget.itemsPrices.Count; i++)
+		{
+			itemsFolded.Add(false);
+		}
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.LabelField("Item Prices", EditorStyles.boldLabel);
+
+		int index = 0;
+		foreach (ItemsPrices.ItemDescription item in myTarget.itemsPrices)
+		{
+			ShowItem(item, index);
+			index++;
+		}
+
+
+		serializedObject.ApplyModifiedProperties();
+		EditorUtility.SetDirty(target);
+	}
+
+	void ShowItem(ItemsPrices.ItemDescription item, int index)
+	{
+		itemsFolded[index] = EditorGUILayout.Foldout(itemsFolded[index], item.item.ToString(), true);
+
+		if (itemsFolded[index++])
+		{
+			item.item = (ItemType)EditorGUILayout.EnumPopup("Item:", item.item);
+			item.price = EditorGUILayout.IntField("Price ($):", item.price);
+			if (item.price < 1)
+				item.price = 1;
+		}
+	}
+}
---
+++ Assets/Editor/OrePricesEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(OresPrices))]
+public class OrePricesEditor : Editor
+{
+	OresPrices myTarget;
+	protected List<bool> oreFolded;
+
+	protected void OnEnable()
+	{
+		myTarget = (OresPrices)target;
+		oreFolded = new List<bool>();
+		for (int i = 0; i < myTarget.oresPrices.Count; i++)
+		{
+			oreFolded.Add(false);
+		}
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.LabelField("Ore Prices", EditorStyles.boldLabel);
+
+		int index = 0;
+		foreach (OresPrices.OreIntPair ore in myTarget.oresPrices)
+		{
+			ShowOre(ore, index);
+			index++;
+		}
+
+
+		serializedObject.ApplyModifiedProperties();
+		EditorUtility.SetDirty(target);
+	}
+
+	void ShowOre(OresPrices.OreIntPair ore, int index)
+	{
+		oreFolded[index] = EditorGUILayout.Foldout(oreFolded[index], ore.ore.ToString(), true);
+
+		if (oreFolded[index++])
+		{
+			ore.ore = (Ore)EditorGUILayout.EnumPopup("Ore:", ore.ore);
+			ore.price = EditorGUILayout.IntField("Price ($):", ore.price);
+			if (ore.price < 1)
+				ore.price = 1;
+		}
+	}
+}
